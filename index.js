7242ed1e112958c2262bb6bad45bacc835f1827c
Kevin Hallett <tauren125@gmail.com>
2019-01-26 17:44:25 -0600

    add pro and con commands

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
+const List = require("./lib/list");
+
 class Procon {
   static run(args, flags) {
     const [command, ...commandArgs] = args;
-    const fileName = flags.fileName;
-    switch (command) {
+    const fileName = (flags.file !== true && flags.file) || "list.json";
+    switch (command.toUpperCase()) {
       case "PRO":
+        return Procon.addToList(
+          fileName,
+          List.SUBLISTS.PRO,
+          commandArgs.join(" ")
+        );
+      case "CON":
+        return Procon.addToList(
+          fileName,
+          List.SUBLISTS.CON,
+          commandArgs.join(" ")
+        );
+      default:
+        return Promise.reject();
     }
+  }
+
+  static addToList(fileName, sublist, msg) {
+    const list = new List(fileName);
+    return list.addToList(sublist, msg);
   }
 }
 
