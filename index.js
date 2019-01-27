95dd09f71cd3174a08c3e31923855a14cf9f1337
Kevin Hallett <tauren125@gmail.com>
2019-01-26 18:31:02 -0600

    add error handling

--- index.js
+++ index.js
@@ -3,15 +3,21 @@ const { PRO, CON } = require("./lib/constants");
 
 class Proconner {
   static run(args, flags) {
-    const [command, ...commandArgs] = args;
+    const [rawCommand, ...commandArgs] = args;
     const fileName = (flags.file !== true && flags.file) || "list.json";
-    switch (command.toUppercase()) {
+    const command = rawCommand && rawCommand.toUppercase();
+    if (!command) {
+      return Promise.reject(
+        new Error("provide a command or use proconner --help to view commands")
+      );
+    }
+    switch (command) {
       case "PRO":
         return Proconner.addToList(fileName, PRO, commandArgs.join(" "));
       case "CON":
         return Proconner.addToList(fileName, CON, commandArgs.join(" "));
       default:
-        return Promise.reject();
+        return Promise.reject(new Error("Invalid command passed " + command));
     }
   }
 
