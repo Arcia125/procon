a723723c427a4d136e52e364126ffb5d2eb98867
Kevin Hallett <tauren125@gmail.com>
2019-01-26 18:38:40 -0600

    move Proconner class

--- index.js
+++ index.js
@@ -1,30 +1,3 @@
-const List = require("./lib/list");
-const { PRO, CON } = require("./lib/constants");
-
-class Proconner {
-  static run(args, flags) {
-    const [rawCommand, ...commandArgs] = args;
-    const fileName = (flags.file !== true && flags.file) || "list.json";
-    const command = rawCommand && rawCommand.toUppercase();
-    if (!command) {
-      return Promise.reject(
-        new Error("provide a command or use proconner --help to view commands")
-      );
-    }
-    switch (command) {
-      case "PRO":
-        return Proconner.addToList(fileName, PRO, commandArgs.join(" "));
-      case "CON":
-        return Proconner.addToList(fileName, CON, commandArgs.join(" "));
-      default:
-        return Promise.reject(new Error("Invalid command passed " + command));
-    }
-  }
-
-  static addToList(fileName, sublist, msg) {
-    const list = new List(fileName);
-    return list.addToList(sublist, msg);
-  }
-}
+const { Proconner } = require("./lib/proconner");
 
 module.exports = Proconner;
